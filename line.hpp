38179f559b9fab55aee35c3250226874e924e7a6
madina9997 <madina9997@mail.ru>
2017-06-29 13:54:04 +0300

    Create line.hpp

---
+++ line.hpp
@@ -0,0 +1,16 @@
+//
+// Created by madina on 21.05.17.
+//
+
+#ifndef LAB4_LINE_HPP
+#define LAB4_LINE_HPP
+
+
+class line {
+public:
+    double x,y, x2,y2;
+    line(double x, double y, double x2, double y2);
+};
+
+
+#endif //LAB4_LINE_HPP
