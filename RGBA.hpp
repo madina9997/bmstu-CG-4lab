b594ed4deed5dd68f947ce7bb5f479c097abce28
madina9997 <madina9997@mail.ru>
2017-06-29 13:51:39 +0300

    Create RGBA.hpp

---
+++ RGBA.hpp
@@ -0,0 +1,16 @@
+//
+// Created by madina on 21.05.17.
+//
+
+#ifndef LAB4_RGBA_HPP
+#define LAB4_RGBA_HPP
+
+
+#include <GL/glew.h>
+
+class RGBA {
+public:
+	GLbyte R,G,B,A;
+};
+
+#endif //LAB4_RGBA_HPP
